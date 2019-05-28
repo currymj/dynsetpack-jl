9e1ba57bdcaa91d056cc1516348c724fa0b92846
Michael J. Curry <curry@cs.umd.edu>
2019-05-28 16:22:18 -0400

    refactoring to have methods on agents

--- matcher_rl.jl
+++ matcher_rl.jl
@@ -14,13 +14,15 @@ mutable struct MLPAgent
     saved_log_probs::Array{Tracker.TrackedReal}
 end
 
+struct GreedyAgent
+end
 MLPAgent(model) = MLPAgent(model, ADAM(0.01), Float32[], Tracker.TrackedReal[])
 
 function Distributions.isprobvec(p::TrackedArray)
     sum(p).data ≈ 1.0
 end
 
-function select_action(state)
+function select_action!(agent::MLPAgent, state)
     probs = agent.nnmodel(state)
     dist = Categorical(softmax(probs))
     action = rand(dist)
@@ -30,11 +32,11 @@ function select_action(state)
 end
 
 
-function remember_reward(reward)
+function remember_reward!(agent::MLPAgent, reward)
     push!(agent.rewards, reward)
 end
 
-function finish_episode(gamma=0.99)
+function finish_episode!(agent::MLPAgent; gamma=0.99)
     # change gamma to be somewhere else later
     returns = Float32[]
     rr = 0.0
@@ -53,16 +55,16 @@ function finish_episode(gamma=0.99)
     empty!(agent.rewards)
 end
 
-function episodeloop(m::MatcherEnv, select_action, remember_reward; nsteps=100)
+function episodeloop(m::MatcherEnv, agent; nsteps=100)
     starttime = time()
     state = reset!(m)
     episodereward = 0.0
     reward = 0.0
     for step=1:nsteps
-        action = select_action(state)
+        action = select_action!(agent, state)
         state, reward, _ = step!(m, action)
         episodereward += reward
-        remember_reward(reward)
+        remember_reward!(agent, reward)
         #println(state)
     end
     endtime = time()
@@ -72,7 +74,7 @@ end
 
 using Printf: @printf
 
-function status_callback(ep, epreward, runningreward)
+function print_probs(agent::MLPAgent, ep, epreward, runningreward)
     testmode!(agent.nnmodel)
     @printf("Episode %d current reward %f running reward %f\n", ep, epreward, runningreward)
     println(softmax(agent.nnmodel([1.0,1.0,1.0,1.0,1.0])))
@@ -80,26 +82,33 @@ function status_callback(ep, epreward, runningreward)
     testmode!(agent.nnmodel, false)
 end
 
-function trainloop(m::MatcherEnv, select_action, remember_reward, finish_episode; nsteps=100, nepisodes=10)
+function print_only(agent, ep, epreward, runningreward)
+    @printf("Episode %d current reward %f running reward %f\n", ep, epreward, runningreward)
+end
+
+function trainloop(m::MatcherEnv, agent; nsteps=100, nepisodes=10, status_callback=print_only)
     runningreward = 0.0
     eprewards = Float32[]
     runningrewards = Float32[]
 
     for ep=1:nepisodes
-        epreward = episodeloop(m, select_action, remember_reward; nsteps=nsteps)
-        finish_episode()
+        epreward = episodeloop(m, agent; nsteps=nsteps)
+        finish_episode!(agent)
         runningreward = 0.05 * epreward + (1 - 0.05) * runningreward
         push!(eprewards, epreward)
         push!(runningrewards, runningreward)
         if ep % 10 == 0
-            status_callback(ep, epreward, runningreward)
+            status_callback(agent, ep, epreward, runningreward)
         end
     end
     eprewards, runningrewards
 end
 
 env = ToyMatcherEnv()
-
 agent = MLPAgent(Chain(Dense(5, 128, relu), Dropout(0.6), Dense(128, 2)))
 plotweights(agent) = heatmap(collect(params(agent.nnmodel[1]))[1].data)
-(eprewards, runningrewards) = trainloop(env, select_action, remember_reward, finish_episode; nsteps=40, nepisodes=1000)
+(eprewards, runningrewards) = trainloop(env, agent; nsteps=40, nepisodes=1000, status_callback=print_probs)
+
+env = BloodTypeMatcherEnv()
+agent = MLPAgent(Chain(Dense(16, 128, relu), Dropout(0.6), Dense(128, 2)))
+(eprewards, runningrewards) = trainloop(env, agent; nsteps=40, nepisodes=10)
