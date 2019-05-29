0a7ea32839eb2aee1c44f23e6680b11059114af0
Michael J. Curry <curry@cs.umd.edu>
2019-05-29 12:57:16 -0400

    split files

---
+++ run_rl.jl
@@ -0,0 +1,20 @@
+include("./matcher_rl.jl")
+
+env = ToyMatcherEnv()
+agent = MLPAgent(Chain(Dense(5, 128, relu), Dropout(0.6), Dense(128, 2)))
+plotweights(agent) = heatmap(collect(params(agent.nnmodel[1]))[1].data)
+(eprewards, runningrewards) = trainloop(env, agent; nsteps=40, nepisodes=1000, status_callback=print_probs)
+test_rewards = [evalepisode(env, agent; nsteps=40) for i=1:20]
+
+env = ToyMatcherEnv()
+agent = GreedyAgent()
+(eprewards, runningrewards) = trainloop(env, agent; nsteps=40, nepisodes=1000)
+test_rewards = [evalepisode(env, agent; nsteps=40) for i=1:20]
+
+env = BloodTypeMatcherEnv()
+agent = MLPAgent(Chain(Dense(16, 128, relu), Dropout(0.6), Dense(128, 2)))
+(eprewards, runningrewards) = trainloop(env, agent; nsteps=40, nepisodes=100)
+
+env = BloodTypeMatcherEnv()
+agent = GreedyAgent()
+(eprewards, runningrewards) = trainloop(env, agent; nsteps=40, nepisodes=100)
