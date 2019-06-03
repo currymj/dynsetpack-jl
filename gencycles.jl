6a1d0528990bff8a28a89786523b992691f2b41e
Michael J. Curry <curry@cs.umd.edu>
2019-06-03 12:40:09 -0400

    code to generate cycles

---
+++ gencycles.jl
@@ -0,0 +1,24 @@
+function twocycles(n_types; n_generate=100)
+    candidates = zeros(Int64, n_types, n_generate)
+
+    for i=1:n_cycles
+        inds = rand(1:n_types, 2)
+        candidates[inds[1], i] += 1
+        candidates[inds[2], i] += 1
+    end
+
+    feasible_sets = unique(candidates; dims=2)
+end
+
+function threecycles(n_types; n_generate=100)
+    candidates = zeros(Int64, n_types, n_generate)
+
+    for i=1:n_cycles
+        inds = rand(1:n_types, 3)
+        candidates[inds[1], i] += 1
+        candidates[inds[2], i] += 1
+        candidates[inds[3], i] += 1
+    end
+
+    feasible_sets = unique(candidates; dims=2)
+end
